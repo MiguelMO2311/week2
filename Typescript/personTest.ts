190516b4f3e1c355f57018bcb5aa9cc489aea62e
MiguelMO2311 <mmeneses73@gmail.com>
2023-11-05 01:10:16 +0100

    confirmacion cambios archivo

---
+++ Typescript/personTest.ts
@@ -0,0 +1,6 @@
+import { Person } from "./person";
+
+const Person2 = new Person('Almudena', 48, 'calle Cañada 101 Palencia');
+
+console.log(Person2.printName())
+console.log(Person2.yearOfBirth())
