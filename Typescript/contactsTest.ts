0f839c9a5360f9d96a788f97b9af25f7d6ee7e16
MiguelMO2311 <mmeneses73@gmail.com>
2023-11-05 01:08:36 +0100

    confirmacion cambios archivo

---
+++ Typescript/contactsTest.ts
@@ -0,0 +1,8 @@
+import { Contacts } from "./contacts";
+import { Person } from "./person";
+
+let people2 = new Person ('Pepe', 40, 'Pza. España 10 Madrid');
+
+console.log (people2.printName());
+console.log (people2.age);
+
