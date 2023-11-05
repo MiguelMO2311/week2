445938edcaeb576733557add31509dfe4ef2a16e
MiguelMO2311 <mmeneses73@gmail.com>
2023-11-05 01:07:56 +0100

    confirmacion cambios archivo

---
+++ Typescript/bookTest.ts
@@ -0,0 +1,8 @@
+// Reto 5 TS.
+import { Book } from "./book";
+
+let booking = new Book ('Cien años de soledad', 496, '9788497592208','Gabriel García Márquez', 'Debolsillo')
+
+console.log (booking.toString());
+
+
