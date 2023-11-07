c5f1827e8b6e96f73d666f6c218009d88209396d
MiguelMO2311 <mmeneses73@gmail.com>
2023-11-07 19:06:21 +0100

    rectificacion de errores y confirmacion

--- Typescript/contacts.js
+++ Typescript/contacts.js
@@ -14,7 +14,7 @@ var Contacts = /** @class */ (function () {
 exports.Contacts = Contacts;
 // let people1 = new Person ('Maria', 30, 'Pza. España 20 Madrid');
 // let people2 = new Person ('Santiago', 22, 'Pza. Mayor 74 Leganes');
-// console.log (people1.name)
+// let agenda = new Contacts ()
+// agenda.people=[people1, people2];
 // console.log (people1.getAdrress())
-// console.log (people2.name)
 // console.log (people2.yearOfBirth())
